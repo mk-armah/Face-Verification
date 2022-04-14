996f1f04cc5f5d7b92495f31939ad2856e85df1d
MICHAEL KOFI ARMAH <85971733+mk-armah@users.noreply.github.com>
2022-04-14 18:00:05 +0000

    Created using Colaboratory

---
+++ content/drive/MyDrive/Coding-Stuffs/Repository/Face-ID/model.py
@@ -0,0 +1,54 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "faceid.ipynb",
+      "provenance": [],
+      "mount_file_id": "1pIPJ5cfcHqfqYf2nrRZXsWeOukoTMrB4",
+      "authorship_tag": "ABX9TyMM9P96UYR2fr3g/4w/Bmt/",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/mk-armah/Face-Verification/blob/master/content/drive/MyDrive/Coding-Stuffs/Repository/Face-ID/model.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "8iT44e2wD4cT"
+      },
+      "outputs": [],
+      "source": [
+        "import os"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "! python /content/drive/MyDrive/Coding-Stuffs/Repository/Face-ID/model.py"
+      ],
+      "metadata": {
+        "id": "h12uphTMNIP3"
+      },
+      "execution_count": 6,
+      "outputs": []
+    }
+  ]
+}
